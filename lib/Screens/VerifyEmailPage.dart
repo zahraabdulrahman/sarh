d2df2cfcf4ee317ab82c9a16cd576254f266e12a
Rana <392206277@qu.edu.sa>
2024-03-10 00:11:52 +0300

    Updating register and sign and and adding verfication email page.

---
+++ lib/Screens/VerifyEmailPage.dart
@@ -0,0 +1,96 @@
+import 'dart:async';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:sarh/Screens/LogRegPage.dart';
+import 'package:sarh/Screens/Navigation.dart';
+import 'package:sarh/reusable_widgets/reusable_widget.dart';
+
+class VerifyEmailPage extends StatefulWidget {
+  const VerifyEmailPage({super.key});
+
+  @override
+  State<VerifyEmailPage> createState() => _VerifyEmailPageState();
+}
+
+class _VerifyEmailPageState extends State<VerifyEmailPage> {
+  bool isEmailVerified = false;
+  bool canResendEmail = false;
+  Timer? timer;
+  @override
+  void initState() {
+    super.initState();
+
+    isEmailVerified = FirebaseAuth.instance.currentUser!.emailVerified;
+    if (!isEmailVerified) {
+      sendVerificationEmail();
+
+      timer = Timer.periodic(
+        const Duration(seconds: 3),
+        (_) => checkEmailVerified(),
+      );
+    }
+  }
+
+  @override
+  void dispose() {
+    timer?.cancel();
+    super.dispose();
+  }
+
+  Future checkEmailVerified() async {
+    await FirebaseAuth.instance.currentUser!.reload();
+
+    setState(() {
+      isEmailVerified = FirebaseAuth.instance.currentUser!.emailVerified;
+    });
+    if (isEmailVerified) timer?.cancel();
+  }
+
+  Future sendVerificationEmail() async {
+    try {
+      final user = FirebaseAuth.instance.currentUser!;
+      await user.sendEmailVerification();
+
+      setState(() => canResendEmail = false);
+      await Future.delayed(const Duration(seconds: 5));
+      setState(() => canResendEmail = true);
+    } catch (e) {
+      // Utils.showSnackBar(e.toString());
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) => isEmailVerified
+      ? const Navigation()
+      : Scaffold(
+          appBar: AppBar(
+            title: const Text("Verify Email"),
+          ),
+          body: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  'يرجى تأكيد الايميل بالرابط المرسل على',
+                  style: TextStyle(fontSize: 20),
+                  textAlign: TextAlign.center,
+                ),
+                firebaseUIButton(context, "اعادة الارسال", () {
+                  onPressed: canResendEmail ? sendVerificationEmail : Null;
+                }),
+                ElevatedButton(
+                child: const Text("الغاء"),
+                onPressed: () {
+                  FirebaseAuth.instance.signOut().then((value) {
+                    print("Signed Out");
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const LogRegPage()));
+                  });
+                })
+              ],
+            ),
+          ));
+}
--- lib/reusable_widgets/reusable_widget.dart
+++ lib/reusable_widgets/reusable_widget.dart
@@ -1,35 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+// import 'package:xyz/utils/pixel_sizes.dart';
 
-TextField reusableTextField(String text, IconData icon, bool isPasswordType,
-    TextEditingController controller) {
+TextField reusableTextField(
+  String text,
+  IconData icon,
+  bool isPasswordType,
+  TextEditingController controller) {
   return TextField(
-    controller: controller,
-    obscureText: isPasswordType,
-    enableSuggestions: !isPasswordType,
-    autocorrect: !isPasswordType,
-    cursorColor: Colors.white,
-    style: TextStyle(color: Colors.white.withOpacity(0.9)),
-    decoration: InputDecoration(
-      contentPadding: const EdgeInsets.symmetric(vertical: -5),
-      prefixIcon: Icon(
-        icon,
-        color: Colors.white70,
+      controller: controller,
+      obscureText: isPasswordType,
+      enableSuggestions: !isPasswordType,
+      autocorrect: !isPasswordType,
+      cursorColor: Colors.white,
+      style: TextStyle(color: Colors.grey.withOpacity(0.9)),
+      decoration: InputDecoration(
+        contentPadding: const EdgeInsets.symmetric(vertical: -5),
+        prefixIcon: Icon(
+          icon,
+          color: Colors.grey,
+        ),
+        labelText: text,
+        labelStyle: TextStyle(color: Colors.grey.withOpacity(0.9)),
+        filled: true,
+        floatingLabelBehavior: FloatingLabelBehavior.never,
+        fillColor: Colors.white.withOpacity(0.3),
+        focusedBorder: const UnderlineInputBorder(
+            borderSide: BorderSide(color: Color(0xFFA7E8BD))),
       ),
-      labelText: text,
-      labelStyle: TextStyle(color: Colors.white.withOpacity(0.9)),
-      filled: true,
-      floatingLabelBehavior: FloatingLabelBehavior.never,
-      fillColor: Colors.white.withOpacity(0.3),
-      focusedBorder: const UnderlineInputBorder(
-          borderSide: BorderSide(color: Color(0xFFA7E8BD))),
-    ),
-    keyboardType: isPasswordType
-        ? TextInputType.visiblePassword
-        : TextInputType.emailAddress,
-  );
+      keyboardType: isPasswordType
+          ? TextInputType.visiblePassword
+          : TextInputType.emailAddress,
+      // onChanged: (value) {
+        // if (confirmationController != null) {
+        //   print("Confirm you password");
+        //   // Check if password and confirmation match
+        //   if (isPasswordType && value != confirmationController.text) {
+        //     print("The passowrd does not match");
+        //     // Set error if passwords do not match
+        //     confirmationController.clear(); // Clear confirmation field
+        //     confirmationController.text =
+        //         value; // Set text again to trigger error message
+        //   }
+        // }
+      );
 }
 
-Container signInSignUpButton(BuildContext context, bool isLogin, Function onTap) {
+Container firebaseUIButton(
+    BuildContext context, String title, Function onTap){
   return Container(
     width: 250,
     height: 50,
@@ -37,29 +55,45 @@ Container signInSignUpButton(BuildContext context, bool isLogin, Function onTap)
     decoration: BoxDecoration(borderRadius: BorderRadius.circular(28)),
     child: ElevatedButton(
       onPressed: () {
-        onTap();
+         onTap();
       },
-      child: Text(
-        isLogin? 'اكمال':'تسجيل الدخول',
-        style: const TextStyle(
-          color: Colors.black, fontWeight: FontWeight.bold, fontSize: 16
-        ),
-      ),
       style: ButtonStyle(
           backgroundColor: MaterialStateProperty.resolveWith((states) {
             if (states.contains(MaterialState.pressed)) {
-              return Color.fromARGB(255, 6, 52, 0);
+              return const Color.fromARGB(255, 6, 52, 0);
             }
             return const Color(0xFFA7E8BD);
           }),
           shape: MaterialStateProperty.all<RoundedRectangleBorder>(
               RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)))),
-    //   child: Text(
-    //     title,
-    //     style: const TextStyle(
-    //         color: Colors.black87, fontWeight: FontWeight.bold, fontSize: 16),
-    //   ),
-    // ),
+      child: Text(
+        title,
+        style: const TextStyle(
+            color: Colors.black, fontWeight: FontWeight.bold, fontSize: 16),
+      ),
+      //   child: Text(
+      //     title,
+      //     style: const TextStyle(
+      //         color: Colors.black87, fontWeight: FontWeight.bold, fontSize: 16),
+      //   ),
+      // ),
     ),
   );
+}
+
+void showDatePickerOnTextField(
+    BuildContext context, TextEditingController dateInput) async {
+  DateTime? pickedDate = await showDatePicker(
+    context: context,
+    initialDate: DateTime.now(),
+    firstDate: DateTime(2000),
+    lastDate: DateTime(2101),
+  );
+
+  if (pickedDate != null) {
+    String formattedDate = DateFormat('yyyy-MM-dd').format(pickedDate);
+    dateInput.text = formattedDate;
+  } else {
+    print("Date is not selected");
+  }
 }
