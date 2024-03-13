c162d9f3974249cc8a9be7a41a1af61f971f2a09
zahra <alzahra@live.com>
2024-03-13 08:11:29 +0300

    update on cards page

--- lib/Screens/profilePage.dart
+++ lib/Screens/profilePage.dart
@@ -68,14 +68,58 @@ class ProfilePage extends StatelessWidget {
               width: 133,
               decoration: BoxDecoration(
                 color: Colors.grey
-              )),
+              ),
+              child: Column(
+                children: [
+
+                  AspectRatio(
+                    aspectRatio: 1.0,
+                    child: Container(
+                      child: Image.asset(
+                        'assets/images/emoji.png',
+                        fit: BoxFit.cover,),
+
+                    ),
+                  ),
+                  Expanded(
+                      flex: 1,
+                      child: Container(
+                          child: Text("مشاعر",
+                            style: TextStyle(
+                              fontSize: 18,
+                            ),)
+                      ))
+                ],
+              ),),
               SizedBox(width: 60),
               Container(
                   height: 166,
                   width: 133,
                   decoration: BoxDecoration(
-                      color: Colors.grey
+                      color: Color(0xFFEFA7A7)
                   ),
+                child: Column(
+                  children: [
+
+                      AspectRatio(
+                        aspectRatio: 1.0,
+                        child: Container(
+                            child: Image.asset(
+                                'assets/images/sentences.PNG',
+                              fit: BoxFit.cover,),
+
+                        ),
+                      ),
+                    Expanded(
+                        flex: 1,
+                        child: Container(
+                          child: Text("جمل محفوظة",
+                            style: TextStyle(
+                              fontSize: 18,
+                            ),)
+                        ))
+                  ],
+                ),
             )
           ]),
               SizedBox(height: 60),
@@ -86,14 +130,58 @@ class ProfilePage extends StatelessWidget {
                         height: 166,
                         width: 133,
                         decoration: BoxDecoration(
-                            color: Colors.grey
-                        )),
+                            color: Color(0xFFC7EAE4)
+                        ),
+                      child: Column(
+                        children: [
+
+                          AspectRatio(
+                            aspectRatio: 1.0,
+                            child: Container(
+                              child: Image.asset(
+                                'assets/images/arabic-food.jpg',
+                                fit: BoxFit.cover,),
+
+                            ),
+                          ),
+                          Expanded(
+                              flex: 1,
+                              child: Container(
+                                  child: Text("طعام",
+                                    style: TextStyle(
+                                      fontSize: 18,
+                                    ),)
+                              ))
+                        ],
+                      ),),
                     SizedBox(width: 60),
                     Container(
                       height: 166,
                       width: 133,
                       decoration: BoxDecoration(
-                          color: Colors.grey
+                          color: Color(0xFFFFD972)
+                      ),
+                      child: Column(
+                        children: [
+
+                          AspectRatio(
+                            aspectRatio: 1.0,
+                            child: Container(
+                              child: Image.asset(
+                                'assets/images/stuff.png',
+                                fit: BoxFit.cover,),
+
+                            ),
+                          ),
+                          Expanded(
+                              flex: 1,
+                              child: Container(
+                                  child: Text("أشياء",
+                                    style: TextStyle(
+                                      fontSize: 18,
+                                    ),)
+                              ))
+                        ],
                       ),
                     )
                   ]),
@@ -106,13 +194,57 @@ class ProfilePage extends StatelessWidget {
                         width: 133,
                         decoration: BoxDecoration(
                             color: Colors.grey
-                        )),
+                        ),
+                      child: Column(
+                        children: [
+
+                          AspectRatio(
+                            aspectRatio: 1.0,
+                            child: Container(
+                              child: Image.asset(
+                                'assets/images/clothes.jpg',
+                                fit: BoxFit.cover,),
+
+                            ),
+                          ),
+                          Expanded(
+                              flex: 1,
+                              child: Container(
+                                  child: Text("ملابس",
+                                    style: TextStyle(
+                                      fontSize: 18,
+                                    ),)
+                              ))
+                        ],
+                      ),),
                     SizedBox(width: 60),
                     Container(
                       height: 166,
                       width: 133,
                       decoration: BoxDecoration(
                           color: Colors.grey
+                      ),
+                      child: Column(
+                        children: [
+
+                          AspectRatio(
+                            aspectRatio: 1.0,
+                            child: Container(
+                              child: Image.asset(
+                                'assets/images/water.png',
+                                fit: BoxFit.cover,),
+
+                            ),
+                          ),
+                          Expanded(
+                              flex: 1,
+                              child: Container(
+                                  child: Text("مشروبات",
+                                    style: TextStyle(
+                                      fontSize: 18,
+                                    ),)
+                              ))
+                        ],
                       ),
                     )
                   ]),
@@ -125,13 +257,43 @@ class ProfilePage extends StatelessWidget {
                         width: 133,
                         decoration: BoxDecoration(
                             color: Colors.grey
-                        )),
+                        ),
+                      child: Column(
+                        children: [
+
+                          AspectRatio(
+                            aspectRatio: 1.0,
+                            child: Container(
+                              child: Image.asset(
+                                'assets/images/action.PNG',
+                                fit: BoxFit.cover,),
+
+                            ),
+                          ),
+                          Expanded(
+                              flex: 1,
+                              child: Container(
+                                  child: Text("أفعال",
+                                    style: TextStyle(
+                                      fontSize: 18,
+                                    ),)
+                              ))
+                        ],
+                      ),
+                    ),
                     SizedBox(width: 60),
                     Container(
                       height: 166,
                       width: 133,
                       decoration: BoxDecoration(
                           color: Colors.grey
+                      ),
+                      child: Column(
+                        children: [
+                          Center(
+                            child: Text("إضافة ملف جديد")
+                          )
+                        ],
                       ),
                     )
                   ]),
