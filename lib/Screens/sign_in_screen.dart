0a1ed4e5fcc330e19ed24a77047df9ab1780f9bb
rana-39 <392206277@qu.edu.sa>
2024-03-24 04:11:52 +0300

    add image upload in register page and update of sign in and forgot passowrd page.

--- lib/Screens/sign_in_screen.dart
+++ lib/Screens/sign_in_screen.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:sarh/Screens/Navigation.dart';
 import 'package:sarh/Screens/register.dart';
 // import 'package:firebase_auth/firebase_auth.dart';
@@ -8,6 +9,8 @@ import 'package:sarh/reusable_widgets/reusable_widget.dart';
 // import 'package:firebase_signin/screens/signup_screen.dart';
 // import 'package:firebase_signin/utils/color_utils.dart';
 import 'package:flutter/material.dart';
+
+import 'forgot_pw_page.dart';
 
 class Sign_in_screen extends StatefulWidget {
   const Sign_in_screen({super.key});
@@ -34,251 +37,101 @@ class _Sign_in_screenState extends State<Sign_in_screen> {
           crossAxisAlignment: CrossAxisAlignment.end,
           children: [
             const SizedBox(height: 350),
-            const Row(children: [
-              SizedBox(
-                width: 177,
-              ),
-              Text(
-                'تسجيل الدخول',
-                style: TextStyle(
-                  fontSize: 28.0,
-                  fontWeight: FontWeight.bold,
-                  color: Color(0xFF3A3A3A),
+             Row(children: [
+              Transform(
+                transform: Matrix4.translationValues(-10, 0.0, 0.0),
+                child: Text(
+                  'تسجيل الدخول',
+                  style: TextStyle(
+                    fontSize: 28.0,
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xFF3A3A3A),
+                  ),
                 ),
               )
             ]),
             const SizedBox(
-              width: 100,
               height: 10,
             ),
-            const Row(
+
+            Row(
               children: [
-                SizedBox(
-                  width: 258,
-                ),
-<<<<<<< HEAD
-                const SizedBox(
-                  width: 100,
-                  height: 15,
-                ),
-
-                  Row(
-                  children: [
-                    Transform(
-                      transform: Matrix4.translationValues(-10, 0.0, 0.0),
-                      child: const Text(
-                        "كلمة السر",
-                        style: TextStyle(
-                          color: Color(0xFFB9B9B9),
-                        ),
-                      ),
-                    )
-                  ],
-                ),
-                SizedBox(
-                  height: 40,
-                  child: reusableTextField(
-                      "ادخل كلمة السر", Icons.lock, false, _passwordTextController),
-                ),
-
-                const SizedBox(
-                  height: 20,
-                ),
-                 Row(
-                  children: [
-                    Transform(
-                      transform: Matrix4.translationValues(-10, 0.0, 0.0),
-                      child: GestureDetector(
-                        onTap: (){
-                          Navigator.push(context,
-                              MaterialPageRoute(builder: (context){
-                            return const forgot_pw_page();
-                          }));
-                        },
-                        child: const Text(
-                          "نسيت كلمة المرور؟",
-                          style: TextStyle(
-                            color: Color(0xFF9ED9AE),
-                          ),
-                        ),
-                      ),
-                    )
-                  ],
-                ),
-                const SizedBox(
-                  height: 35,
-                ),
-                Center(
-                  child: firebaseUIButton(context, "تسجيل الدخول", () {
-                        FirebaseAuth.instance
-                            .signInWithEmailAndPassword(
-                                email: _emailTextController.text,
-                                password: _passwordTextController.text)
-                            .then((value) {
-                          Navigator.push(context,
-                              MaterialPageRoute(builder: (context) => const Navigation()));
-                        }).onError((error, stackTrace) {
-                          print("Error ${error.toString()}");
-                        });
-                      }),
-                ),
-                signUpOption()
-
-                // const SizedBox(height: 70),
-                // Center(child: signInSignUpButton(context, false, () {})),
-                // signUpOption(),
-                // Padding(
-                //   padding: const EdgeInsets.only(top: 15.0),
-                //   child: Center(
-                //     child: Container(
-                //       decoration: BoxDecoration(
-                //         borderRadius: BorderRadius.circular(10.0),
-                //         color: Colors.amber.shade100
-                //       ),
-                //       height: 60,
-                //       width: 250,
-                //       child: CupertinoButton(
-                //         child: const Text('تسجيل الدخول', style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold,fontSize: 20),),
-                //         onPressed: () {
-                //           Navigator.push(
-                //             context,
-                //             CupertinoPageRoute(
-                //                 builder: (context) => const Navigation()),
-                //           );
-                //         },
-                //       ),
-                //     ),
-                //   ),
-                // ),
-=======
-                Text(
-                  "البريد الالكتروني",
-                  style: TextStyle(
-                    color: Color(0xFFB9B9B9),
+                Transform(
+                  transform: Matrix4.translationValues(-10, 0.0, 0.0),
+                  child: const Text(
+                    "البريد الالكتروني",
+                    style: TextStyle(
+                      color: Color(0xFFB9B9B9),
+                    ),
                   ),
                 )
->>>>>>> 45a484bed783e2e0ecae6087b53966ae2445a0ce
               ],
             ),
-            reusableTextField("ادخل بريدك الالكتروني", Icons.email, false,
-                _emailTextController),
+
+            reusableTextField("ادخل بريدك الالكتروني", Icons.email, false,_emailTextController),
+
             const SizedBox(
-              width: 100,
               height: 10,
             ),
-            // Row(
-            //   children: [
-            //     const SizedBox(width: 30,),
-            //     Container(
-            //       height: 49, width: 315,
-            //       alignment: Alignment.centerRight,
-            //       child: const TextField(
-            //         textDirection: TextDirection.rtl,
-            //         decoration: InputDecoration(
-            //           labelText: '',
-            //           hintText: 'ادخل بريدك الالكتروني',
-            //           prefixIcon: Icon(Icons.check),
-            //           border: UnderlineInputBorder(
-            //               borderSide: BorderSide(
-            //                   color: Color(0xFFA7E8BD),
-            //                   width: 1
-            //               )
-            //           ),
-            //         ),
-            //       )),],
-            // ),
-            const Row(
-              children: [
-                SizedBox(
-                  width: 258,
-                  height: 30,
-                ),
-                Text(
-                  "كلمة السر",
-                  style: TextStyle(
-                    color: Color(0xFFB9B9B9),
-                  ),
-                )
-              ],
-            ),
-            reusableTextField(
-                "ادخل كلمة السر", Icons.lock, true, _passwordTextController),
-            // Row(
-            //   children: [
-            //     const SizedBox(width: 30,),
-            //     Container(
-            //         height: 49, width: 315,
-            //         alignment: Alignment.centerRight,
-            //         child: const TextField(
-            //           obscureText: true,
-            //           textDirection: TextDirection.rtl,
-            //           decoration: InputDecoration(
-            //             labelText: '',
-            //             hintText: 'ادخل كلمة السر',
-            //             prefixIcon: Icon(Icons.lock),
-            //             border: UnderlineInputBorder(
-            //                 borderSide: BorderSide(
-            //                     color: Color(0xFFA7E8BD),
-            //                     width: 1
-            //                 )
-            //             ),
-            //           ),
-            //         )),
-            //   ],
-            // ),
-            const Row(
-              children: [
-                SizedBox(
-                  width: 245,
-                ),
-                Text(
-                  "نسيت كلمة المرور؟",
-                  style: TextStyle(
-                    color: Color(0xFF9ED9AE),
-                  ),
-                )
-              ],
-            ),
-            firebaseUIButton(context, "تسجيل الدخول", () {
-                  FirebaseAuth.instance
-                      .signInWithEmailAndPassword(
-                          email: _emailTextController.text,
-                          password: _passwordTextController.text)
-                      .then((value) {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => const Navigation()));
-                  }).onError((error, stackTrace) {
-                    print("Error ${error.toString()}");
-                  });
-                }),
-            signUpOption() 
 
-            // const SizedBox(height: 70),
-            // Center(child: signInSignUpButton(context, false, () {})),
-            // signUpOption(),
-            // Padding(
-            //   padding: const EdgeInsets.only(top: 15.0),
-            //   child: Center(
-            //     child: Container(
-            //       decoration: BoxDecoration(
-            //         borderRadius: BorderRadius.circular(10.0),
-            //         color: Colors.amber.shade100
-            //       ),
-            //       height: 60,
-            //       width: 250,
-            //       child: CupertinoButton(
-            //         child: const Text('تسجيل الدخول', style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold,fontSize: 20),),
-            //         onPressed: () {
-            //           Navigator.push(
-            //             context,
-            //             CupertinoPageRoute(
-            //                 builder: (context) => const Navigation()),
-            //           );
-            //         },
-            //       ),
-            //     ),
-            //   ),
-            // ),
+            Row(
+              children: [
+                Transform(
+                  transform: Matrix4.translationValues(-10, 0.0, 0.0),
+                  child: const Text(
+                    "كلمة السر",
+                    style: TextStyle(
+                      color: Color(0xFFB9B9B9),
+                    ),
+                  ),
+                )
+              ],
+            ),
+
+            reusableTextField("ادخل كلمة السر", Icons.lock, true, _passwordTextController),
+
+            const SizedBox(
+              height: 20,
+            ),
+            Center(
+              child: Row(
+                children: [
+                  Transform(
+                    transform: Matrix4.translationValues(-10, 0.0, 0.0),
+                    child: GestureDetector(
+                      onTap: (){
+                        Navigator.push(context,
+                            MaterialPageRoute(builder: (context){
+                              return forgot_pw_page();
+                            }));
+                      },
+                      child: const Text(
+                        "نسيت كلمة المرور؟",
+                        style: TextStyle(
+                          color: Color(0xFF9ED9AE),
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+
+            Center(
+              child: firebaseUIButton(context, "تسجيل الدخول", () {
+                    FirebaseAuth.instance
+                        .signInWithEmailAndPassword(
+                            email: _emailTextController.text,
+                            password: _passwordTextController.text)
+                        .then((value) {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => const Navigation()));
+                    }).onError((error, stackTrace) {
+                      print("Error ${error.toString()}");
+                    });
+                  }),
+            ),
+            signUpOption() ,
           ],
         ),
       ),
