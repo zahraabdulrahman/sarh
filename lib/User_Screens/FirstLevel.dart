6f23e86d4db73e28cbfeb2f0316a2f6a80c321a7
rana-39 <392206277@qu.edu.sa>
2024-05-23 23:48:13 +0300

    updates

--- lib/User_Screens/FirstLevel.dart
+++ lib/User_Screens/FirstLevel.dart
@@ -5,6 +5,7 @@ import 'package:path_provider/path_provider.dart';
 import 'package:path/path.dart' as path;
 import 'package:http/http.dart' as http;
 import 'dart:io';
+import 'package:permission_handler/permission_handler.dart';
 
 class FirstLevel extends StatefulWidget {
   const FirstLevel({super.key});
@@ -15,7 +16,32 @@ class FirstLevel extends StatefulWidget {
 
 class _FirstLevelState extends State<FirstLevel> {
   AudioPlayer audioPlayer = AudioPlayer();
-  // FlutterSoundRecorder flutterSoundRecorder = FlutterSoundRecorder();
+  FlutterSoundRecorder flutterSoundRecorder = FlutterSoundRecorder();
+  bool isRecorderInitialized = false;
+  bool isRecording = false;
+
+  @override
+  void initState() {
+    super.initState();
+    initRecorder();
+  }
+
+  @override
+  void dispose() {
+    flutterSoundRecorder.closeAudioSession();
+    super.dispose();
+  }
+
+  Future<void> initRecorder() async {
+    await flutterSoundRecorder.openAudioSession();
+    isRecorderInitialized = true;
+    setState(() {});
+
+    final status = await Permission.microphone.request();
+    if (status != PermissionStatus.granted) {
+      throw RecordingPermissionException('Microphone permission not granted');
+    }
+  }
 
   Future<String> getTemporaryDirectoryPath() async {
     final directory = await getTemporaryDirectory();
@@ -27,6 +53,8 @@ class _FirstLevelState extends State<FirstLevel> {
   }
 
   void startRecording() async {
+    if (!isRecorderInitialized) return;
+
     String temporaryPath = await getTemporaryDirectoryPath();
     String filePath = path.join(temporaryPath, 'recording.mp3');
 
@@ -35,14 +63,25 @@ class _FirstLevelState extends State<FirstLevel> {
       codec: Codec.mp3,
     );
 
-    // Pass the file path to the processAudioFile function
+    setState(() {
+      isRecording = true;
+    });
+  }
+
+  void stopRecording() async {
+    if (!isRecorderInitialized) return;
+
+    await flutterSoundRecorder.stopRecorder();
+    setState(() {
+      isRecording = false;
+    });
+
+    String temporaryPath = await getTemporaryDirectoryPath();
+    String filePath = path.join(temporaryPath, 'recording.mp3');
+
     processAudioFile(filePath);
   }
-  void stopRecording() async {
-    await flutterSoundRecorder.stopRecorder();
-    await flutterSoundRecorder.closeAudioSession();
-    print('Recording stopped');
-  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -70,20 +109,20 @@ class _FirstLevelState extends State<FirstLevel> {
       ),
       body: Center(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text("كان يا ما كان",
-              style: TextStyle(color: Colors.black, fontSize: 30),),
-            IconButton(
-              icon: Icon(Icons.volume_up, size: 44,color: Colors.black),
-              onPressed: () {
-                playAudio();
-              },
+            Text(
+              "كان يا ما كان",
+              style: TextStyle(color: Colors.black, fontSize: 30),
             ),
-
             IconButton(
-              icon: Icon(Icons.mic),
+              icon: Icon(Icons.volume_up, size: 44, color: Colors.black),
+              onPressed: playAudio,
+            ),
+            IconButton(
+              icon: Icon(isRecording ? Icons.stop : Icons.mic, size: 44, color: Colors.black),
               onPressed: () {
-                if (flutterSoundRecorder.isRecording) {
+                if (isRecording) {
                   stopRecording();
                 } else {
                   startRecording();
@@ -103,8 +142,8 @@ Future<void> processAudioFile(String filePath) async {
   request.files.add(await http.MultipartFile.fromPath('file', audioFile.path));
   var response = await request.send();
   if (response.statusCode == 200) {
-  print('File uploaded successfully');
+    print('File uploaded successfully');
   } else {
-  print('Failed to upload file');
+    print('Failed to upload file');
   }
 }
